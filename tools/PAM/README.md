65cfcc97a2c8b733594ab666ae337e3bc6447cd3
John Doe <icse19-focus@gmail.com>
2018-08-25 09:25:51 +0200

    Added Pam evaluation tool

---
+++ tools/PAM/README.md
@@ -0,0 +1,25 @@
+# APIMaterials
+
+Parsing PAM: Python code
+
+Python scripts support ten folder validation. parsingPAM.py provides the following functions:
+
+* splitFiles: it takes as argument the folder in which there are all the files that represent the project and takes 2/3 of each and discard 1/3;
+* combineMethodsInv: It creates the arff file with the project combined with the 3 invocations extracted. It takes the folder of the invocations, the folder of the projects and the folder of destination in which there are stored all the arff file ;
+* combineAll: It merges one project with the other 304 in order to create the arff file to be analyzed by PAM. It takes the folder that contains the projects, the folder that contains all the dataset (the 305 projects) and the destination folder;
+* fromFocusToPam: It converts the format used for Focus to the format used by PAM. It takes as argument the folder where are stored the projects in the Focus format and the destination folder.
+* getGroundTruth: It takes the first 3 invocations and discard the others, that are inserted in the ground truth folder. It takes the path in which there are the file to split and the destination folders, one for the testing invocations and for the ground truth.
+
+
+ConvertArffFile.py converts metadatas Focus2PAM. 
+
+The complete evaluation process involves the following sequence of functions to call:
+1. fromFocusToPam
+2. convertArffFile
+3. splitFiles
+4. getGroundTruth
+5. combineMethodsInv
+6. combineAll
+
+
+Special thanks go to @claudioDsi: he supported us during the evaluation of PAM.
--- tools/PAM/parsingPAM.py
+++ tools/PAM/parsingPAM.py
@@ -1,19 +1,18 @@
 import os
-import shutil
-from distutils.dir_util import copy_tree
-def splitFiles():
-    PAM_path="C://Users//claudio//Desktop//PAM_all//"
-    total_row=0
+
+
+def splitFiles(PAM_path,part_path,part2_path):
+
+    #PAM_path="C://Users//claudio//Desktop//dataset//"
     for f in os.listdir(PAM_path):
         print(f)
         file = open(PAM_path+f,"r",encoding='utf-8', errors='ignore')
-
         lines=file.readlines()
         tot=len(lines)
         rmv=(tot*2)/3
         print(int(rmv))
-        part = open("C://Users//claudio//Desktop//folder1//temp//" + f, "w",encoding='utf-8', errors='ignore')
-        part2= open("C://Users//claudio//Desktop//PAM_processed//Round1//removed//" + f, "w",encoding='utf-8', errors='ignore')
+        part = open(part_path + f, "w",encoding='utf-8', errors='ignore')
+        part2= open(part2_path + f, "w",encoding='utf-8', errors='ignore')
         i=0
         c=0
         #part.write("@relation project\n @attribute fqCaller string \n  @attribute fqCalls string \n @data \n")
@@ -34,53 +33,15 @@ def splitFiles():
                 if(c>=tot):
                     break
 
-def getMethodInv():
-    n=0
-    i=0
-    path="C://Users//claudio//Desktop//PAM_processed//Round1//baseline//"
-    pathResults="C://Users//claudio//Desktop//PAM_processed//Round1//TestingInvocations//"
-    #temp=open("temp.txt","w")
-
-    list_inv=[]
-    for f in os.listdir(path):
-        file = open(path + f, "r",encoding='utf-8', errors='ignore')
-        temp=open(pathResults+f,"w",encoding='utf-8', errors='ignore')
-        for line in file.readlines():
-            methodcalls=line[line.find("','")+3:]
-
-            print(methodcalls)
-            n=methodcalls.count(" ")
-            if(n>=4):
-                i=i+1
-                if (i>3):
-                    inv=methodcalls.replace(" ","\n")
-                    print(line[:line.find("','")+3]+inv)
-                    temp.write(inv)
-                    break
 
 
-def groundT(path="C://Users//claudio//Desktop//folder1//TestingInvocations//"):
-    pathResults = "C://Users//claudio//Desktop//folder1//GroundTruth//"
-    gtfile = open("gtfile.txt", "w")
-    calls=open("temp.txt","r")
-    i=0
-    for f in os.listdir(path):
-        calls = open(path + f, "r")
-        gtfile=open(pathResults+f,"w")
-        for lines in calls:
+def combineMethodsInv(root_invocations,root_testing,root_arff):
+    #root_processed="C://Users//claudio//Desktop//second_run//temp//"
+    #root_testing="C://Users//claudio//Desktop//second_run//TestingInvocations//"
+    #root_arff="C://Users//claudio//Desktop//second_run//Arff//"
 
-            i = i + 1
-            if (i > 2):
-                break
-
-
-def combineMethodsInv():
-    root_processed="C://Users//claudio//Desktop//folder1//temp//"
-    root_testing="C://Users//claudio//Desktop//folder1//TestingInvocations//"
-    root_arff="C://Users//claudio//Desktop//folder1//Arff//"
-
-    for f in os.listdir(root_processed):
-        file=open(root_processed+f,"r",encoding='utf-8', errors='ignore')
+    for f in os.listdir(root_invocations):
+        file=open(root_invocations+f,"r",encoding='utf-8', errors='ignore')
         thisFile = os.path.basename(file.name)
         i = thisFile.find(".arff")
         thisFile = thisFile[:i]
@@ -96,171 +57,47 @@ def combineMethodsInv():
             result_arff.write(e2)
 
 
-def combineAll(filename):
-    start=open("C://Users//claudio//Desktop//folder1//Arff//"+filename,"r",encoding="utf-8",errors="ignore")
-    resultArff=open("C://Users//claudio//Desktop//folder1//PAM_arff//"+filename,"w",encoding="utf-8",errors="ignore")
-    for e in start:
-        resultArff.write(e)
-    folders="C://Users//claudio//Desktop//PAM_all//"
-    for e in os.listdir(folders):
-        file=open(folders+e,"r",encoding="utf-8",errors="ignore")
-        for row in file:
-            resultArff.write(row)
-
-
-def renamingFile():
-    root_testing = "C://Users//claudio//Desktop//folder1//TestingInvocations//"
-    root_arff="C://Users//claudio//Desktop//folder1//ex"
-    for f in os.listdir(root_testing):
-        file=open(root_testing+f,"r",encoding="utf-8",errors="ignore")
-        thisFile=os.path.basename(file.name)
-        i=thisFile.find(".txt")
-        thisFile=thisFile[:i]
-        print(thisFile)
-        file = open(root_arff + thisFile+".arff", "w", encoding="utf-8", errors="ignore")
+def combineAll(start,end,dataset):
+    #start="C://Users//claudio//Desktop//second_run//Arff//"
+    #end="C://Users//claudio//Desktop//second_run//PAM_arff//"
+    for file in os.listdir(start):
+        ff =open(start+file,"r",encoding="utf-8",errors="ignore")
+        resultArff=open(end+file,"w",encoding="utf-8",errors="ignore")
+        for e in ff:
+            resultArff.write(e)
+        #folders="C://Users//claudio//Desktop//dataset//"
+        for e in os.listdir(dataset):
+            file=open(dataset+e,"r",encoding="utf-8",errors="ignore")
+            for row in file:
+                resultArff.write(row)
 
 
 
-
-def findMalformed():
-    lpath = "C://Users//claudio//Desktop//Focus//Focus//"
-    lpath2 = "C://Users//claudio//Desktop//PAM_results//"
-    moveTo="C://Users//claudio//Desktop//wrongFormat//"
-    pam=open("pam.txt","w")
-    wrong = open("wrong.txt", "w")
-    errors = open("focus.txt", "w")
-
-    for f in os.listdir(lpath):
-        pamFile=open(lpath+f,"r")
-        name=os.path.basename(pamFile.name)
-        i=name.find(".")
-        name=name[:i]
-        pam.write(name+"\n")
-
-
-    for f2 in os.listdir(lpath2):
-        file=open(lpath2+f2)
-        name2=os.path.basename(file.name)
-        j=name2.find(".")
-        name2=name2[:j]
-
-        wrong.write(name2+"\n")
-    list1=[]
-    list2=[]
-    pam = open("pam.txt", "r")
-    #wrong = open("wrong.txt", "r")
-    lines=pam.readlines()
-    for l in lines:
-        list1.append(l)
-
-    lines2=wrong.readlines()
-
-    for l2 in lines2:
-        list2.append(l2)
-        print(l2)
-
-    res=[x for x in list1 if x not in list2]
-    for e in res:
-        errors.write(e)
-
-def retreiveList():
-    dir="C://Users//claudio//Desktop//left//"
-    err1 = open("err1.txt", "w")
-    err2 = open("err2.txt", "w")
-    final = open("final.txt", "w")
-    l1=[]
-    l2=[]
-    listPhoung = "C://Users//claudio//Desktop//Focus//Focus//"
-    for d in os.listdir(dir):
-        d = open(dir + d)
-        name2 = os.path.basename(d.name)
-        j = name2.find(".")
-        d = name2[:j]
-        l1.append(d)
-        #114
-
-    #lines=listPhoung.readlines()
-    for l in os.listdir(listPhoung):
-        l = open(listPhoung + l)
-        name = os.path.basename(l.name)
-        i = name.find(".")
-        l = name[:i]
-        l2.append(l)
-        #548
-    res1 = [x for x in l1 if x not in l2]
-    for e in res1:
-        err1.write(e+"\n")
-    res2 = [x for x in l2 if x not in l1]
-    for e1 in res2:
-        err2.write(e1 + "\n")
-    err1 = open("err1.txt", "r")
-    err2 = open("err2.txt", "r")
-    lines=err1.readlines()
-    lines2 = err2.readlines()
-    listF=[x for x in lines2 if x not in lines]
-    for f in listF:
-        final.write(f)
-
-def movingFiles():
-    fileSource = open("C://Users//claudio//Desktop//Evaluation//Evaluation//List.txt", "r")
-    lines=fileSource.readlines()
-    listsrc=open("ciao.txt","w")
-
-    for l in lines:
-        j = l.find(".")
-        d = l[:j]
-        listsrc.write(d+"\n")
-
-
-    listsrc = open("ciao.txt", "r")
-
-    src="C://Users//claudio//Desktop//Focus//Focus//"
-    dst="C://Users//claudio//Desktop//PAM_dataset//"
-    print("dsds")
-    for s in listsrc:
-
-        try:
-            fromDirectory = src+ s.rstrip()
-            toDirectory = dst+ s.strip()
-            print(toDirectory)
-            copy_tree(fromDirectory, toDirectory)
-            print("moved")
-        except:
-            print(fromDirectory)
-
-def fromFocusToPam():
-    dirFocus="C://Users//claudio//Desktop//FOCUS//Round1//TestingInvocations//"
-    dest = "C://Users//claudio//Desktop//folder1//TestingInvocations//"
+def fromFocusToPam(dirFocus,dest):
+    #dirFocus="C://Users//claudio//Desktop//PAM_dataset//"
+    #dest = "C://Users//claudio//Desktop//focusToPam//"
 
     for f in os.listdir(dirFocus):
         file = open(dirFocus +f,"r",encoding='utf-8', errors='ignore')
         name=os.path.basename(file.name)
-        lines=file.readlines()
+
         pam = open(dest + name, "w",encoding='utf-8', errors='ignore')
-        try:
-            for l in lines:
-                cleaned=l.replace("#","','")
-                cleaned=cleaned.replace("\n","'")
-                #cleaned=cleaned.replace("$","")
+        for l in file:
+            cleaned = l.replace("#", "','")
+            cleaned = cleaned.replace("\n", "'")
+            # cleaned=cleaned.replace("$","")
+            pam.write("'" + cleaned + "\n")
 
-
-                pam.write("'"+cleaned+"\n")
-        except:
-            continue
-
-def groundT2():
-    PAM_path = "C://Users//claudio//Desktop//PAM_processed//Round1//TestingInvocations//"
+def getGroundTruth(arff_path,invocations_path,groundt_path):
+    #PAM_path = "C://Users//claudio//Desktop//focus_d//"
     total_row = 0
-    for f in os.listdir(PAM_path):
+    for f in os.listdir(arff_path):
         print(f)
-        file = open(PAM_path + f, "r",encoding='utf-8', errors='ignore')
-
+        file = open(arff_path + f, "r",encoding='utf-8', errors='ignore')
         lines = file.readlines()
         tot = len(lines)
-        rmv = (tot * 2) / 3
-        print(int(rmv))
-        part = open("C://Users//claudio//Desktop//PAM_processed//Round1//TestingInvocations//" + f, "w",encoding='utf-8', errors='ignore')
-        part2 = open("C://Users//claudio//Desktop//PAM_processed//Round1//GroundTruth//" + f, "w",encoding='utf-8', errors='ignore')
+        part = open(invocations_path + f, "w",encoding='utf-8', errors='ignore')
+        part2 = open(groundt_path + f, "w",encoding='utf-8', errors='ignore')
         i = 0
         c = 0
         for line in lines:
@@ -270,10 +107,8 @@ def groundT2():
                 break
 
         for line2 in lines:
-
             i += 1
             c += 1
-
             print(c)
             if (c >= 3):
                 part2.write(line2)
@@ -282,54 +117,36 @@ def groundT2():
 
 
 
-def fixFormat():
-    path= "C://Users//claudio//Desktop//PAM_processed//Round1//TestingInvocations//"
-    pathRes="C://Users//claudio//Desktop//PAM_processed//Round1//cleaned//"
+def recommendationFormat(path,destPath):
+    #path="C://Users//claudio//Desktop//second_run//Recommendations//"
+    #destPath="C://Users//claudio//Desktop//PAM_recommendations//"
     for f in os.listdir(path):
-        print(f)
-        file = open(path + f, "r",encoding='utf-8', errors='ignore')
-        result= open(pathRes + f, "w",encoding='utf-8', errors='ignore')
-        i=0
-        lines = file.readlines()
-        for l in lines:
-            l=l.replace("\n","',")
-            result.write("'"+l)
+        file=open(path+f,"r",encoding="utf-8",errors="ignore")
+        dest=open(destPath+f,"w",encoding="utf-8",errors="ignore")
+        list_prob=[]
+        list_rec=[]
+        dict={'rec': "", 'prob': ""}
+
+        for e in file:
+            pos2 = e.find("[")
+            if pos2 != -1:
+                rec = e[pos2 + 1:]
+                rec = rec.replace("]", "")
+                dict['rec'] =rec
+                list_rec.append(dict["rec"])
+            pos = e.find("prob:")
+            if pos != -1:
+                prob=e[pos+6:]
+                dict['prob'] =prob
+                list_prob.append(dict["prob"])
+
+        for i,j in zip(list_rec,list_prob):
+            dest.write(i.replace("\n","  "))
+            dest.write(j)
 
 
 
-def fix2():
-    path = "C://Users//claudio//Desktop//PAM_processed//Round1//cleaned//"
-    pathRes = "C://Users//claudio//Desktop//PAM_processed//Round1//TestingInvocations//"
-    for f in os.listdir(path):
-        print(f)
-        file = open(path + f, "r", encoding='utf-8', errors='ignore')
-        result = open(pathRes + f, "w", encoding='utf-8', errors='ignore')
-        i = 0
-        lines = file.readlines()
-        for l in lines:
-            l = l[:-1]
-            result.write(l)
 
 
-def mvFile():
-    lpath2 = "C://Users//claudio//Desktop//PAM_all//"
-    for f in os.listdir(lpath2):
 
-        shutil.move(lpath2+f,"C://Users//claudio//Desktop//")
-        break
 
-#splitFiles()
-#getMethodInv()
-#groundT2()
-#fixFormat()
-#fix2()
-#combineAll()
-#retreiveList()
-#findMalformed()
-#movingFiles()
-#fromFocusToPam()
-#combineMethodsInv()
-#renamingFile()
-
-#combineAll("0b8674aeae24e0b2d731dd37f1bb8d2f80f6c088.arff")
-mvFile()
