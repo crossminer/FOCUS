0b465cb28ad3a5ba16eb917d1688c57092b1214e
tdegueul <degueule@cwi.nl>
2019-01-12 17:57:30 +0100

    Extracting ValueComparator in its own file

--- tools/Focus/src/main/java/org/focus/Runner.java
+++ tools/Focus/src/main/java/org/focus/Runner.java
@@ -2,30 +2,10 @@ package org.focus;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Comparator;
-import java.util.Map;
 import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-class ValueComparator implements Comparator<String> {
-	Map<String, Float> base;
-
-	public ValueComparator(Map<String, Float> base) {
-		this.base = base;
-	}
-
-	// Note: this comparator imposes orderings that are inconsistent with equals.
-	public int compare(String a, String b) {
-		if (base.get(a) >= base.get(b)) {
-			return -1;
-		} else {
-			return 1;
-		}
-		// returning 0 would merge keys
-	}
-}
 
 public class Runner {
 	private String srcDir;
