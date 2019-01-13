6fa17b4982baf82e502996d095e9eacf26956741
tdegueul <degueule@cwi.nl>
2019-01-13 15:01:13 +0100

    Cleaning ContextAwareRecommendation

--- tools/Focus/src/main/java/org/focus/ContextAwareRecommendation.java
+++ tools/Focus/src/main/java/org/focus/ContextAwareRecommendation.java
@@ -11,7 +11,6 @@ import java.util.TreeMap;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 public class ContextAwareRecommendation {
 	private DataReader reader = new DataReader();
 
@@ -28,183 +27,167 @@ public class ContextAwareRecommendation {
 	private int numOfRows;
 	private int numOfCols;
 	private int numOfSlices;
-	
+
 	private static final Logger log = LogManager.getFormatterLogger(ContextAwareRecommendation.class);
 
-	public ContextAwareRecommendation(String sourceDir, String suFolder, int numOfNeighbours,	int teStartPos, int teEndPos) {			
+	public ContextAwareRecommendation(String sourceDir, String suFolder, int numOfNeighbours, int teStartPos,
+			int teEndPos) {
 		this.srcDir = sourceDir;
-		this.subFolder = suFolder;		
-		this.numOfNeighbours = numOfNeighbours;		
+		this.subFolder = suFolder;
+		this.numOfNeighbours = numOfNeighbours;
 		this.groundTruth = this.srcDir + subFolder + "/" + "GroundTruth" + "/";
 		this.recDir = this.srcDir + subFolder + "/" + "Recommendations" + "/";
 		this.simDir = this.srcDir + subFolder + "/" + "Similarities" + "/";
 		this.testingStartPos = teStartPos;
-		this.testingEndPos = teEndPos;	
+		this.testingEndPos = teEndPos;
 	}
-
 
 	/**
 	 * Build a 3-D user-item-context ratings matrix
 	 */
-	public byte[][][] buildUserItemContextMatrix(String testingPro, List<String> listOfProjects, List<String> listOfMethodInvocations) {		
-		String filename = testingPro;
-		String tmp = this.simDir + filename;		
+	public byte[][][] buildUserItemContextMatrix(String testingPro, List<String> listOfProjects,
+			List<String> listOfMethodInvocations) {
+		Map<Integer, String> simProjects = reader.getMostSimilarProjects(simDir + testingPro, numOfNeighbours);
+		Map<String, Map<String, Set<String>>> allProjects = new HashMap<>();
 
-		Map<Integer, String> simProjects =  reader.getMostSimilarProjects(tmp, numOfNeighbours);		
-		Set<Integer> keySet = simProjects.keySet();			
-		Set<String> keySet2 = null;
-		Map<String,Set<String>> tmpMethodInvocations = new HashMap<String,Set<String>>();
-		Map<String,Map<String,Set<String>>> allProjects = new HashMap<String,Map<String,Set<String>>>();
-
-		Set<String> allMDs = new HashSet<String>();		
-		Set<String> allMIs = new HashSet<String>();
-
-		int sz = keySet.size();
-		// An ordered list of all projects
 		List<String> listOfPRs = new ArrayList<String>();
+		Set<String> allMDs = new HashSet<>();
+		Set<String> allMIs = new HashSet<>();
 
-		for(int key=0;key<sz;key++) {				
-			String project = simProjects.get(key);				
-			filename = project;
-			// tmpMethodInvocations =  reader.getProjectDetails(this.srcDir,filename);
-			tmpMethodInvocations =  reader.getProjectDetailsFromARFF2(this.srcDir,filename);			
+		int sz = simProjects.keySet().size();
+		for (int key = 0; key < sz; key++) {
+			String project = simProjects.get(key);
+			Map<String, Set<String>> tmpMIs = reader.getProjectDetails(srcDir, project);
 
-			// all method declarations of a project/library			
-			keySet2 = tmpMethodInvocations.keySet();						
-			allMDs.addAll(keySet2);
-			for(String key2:keySet2) {			
-				allMIs.addAll(tmpMethodInvocations.get(key2));				
-			}
+			// All method declarations of a project
+			allMDs.addAll(tmpMIs.keySet());
+			for (Set<String> mis : tmpMIs.values())
+				allMIs.addAll(mis);
 
-			allProjects.put(project, tmpMethodInvocations);			
-			listOfPRs.add(project);						
+			allProjects.put(project, tmpMIs);
+			listOfPRs.add(project);
 		}
 
-		// the slice for the testing project is located at the end of the matrix	
+		// The slice for the testing project is located at the end of the matrix
 		listOfPRs.add(testingPro);
 
-		// read the corresponding grouth-truth file to remove the invocations in there		
-		Set<String> groundTruthInvocations = reader.getGroundTruthInvocations(this.groundTruth,testingPro);
-		Map<String,Set<String>> testingMIs = new HashMap<String,Set<String>>();
-		String testingMD = "";
+		// Read the corresponding ground-truth file
+		Set<String> groundTruthMIs = reader.getGroundTruthInvocations(groundTruth, testingPro);
+		Map<String, Set<String>> testingMIs = new HashMap<>();
 
-		// add the testing project, exclude ground-truth method invocations
-		tmpMethodInvocations =  reader.getTestingProjectDetails(this.srcDir,testingPro,groundTruthInvocations,testingMIs);	
+		// Add the testing project, excluding ground-truth method invocations
+		Map<String, Set<String>> tmpMIs = reader.getTestingProjectDetails(srcDir, testingPro, groundTruthMIs,
+				testingMIs);
 
-		Set<String> set1 =  tmpMethodInvocations.keySet();
-		Set<String> set11 = new HashSet<String>();
+		allMDs.addAll(tmpMIs.keySet());
+		for (Set<String> s : tmpMIs.values())
+			allMIs.addAll(s);
 
-		for(String s1:set1)set11.add(s1);
-		allMDs.addAll(set1);
+		// FIXME: ???
+		String testingMD = testingMIs.keySet().iterator().next();
+		Set<String> tmpMISet = testingMIs.get(testingMD);
 
-		for(String s:set1) {			
-			allMIs.addAll(tmpMethodInvocations.get(s));				
-		}
+		// Add testing data to the list of invocations/projects
+		tmpMIs.putAll(testingMIs);
+		allProjects.put(testingPro, tmpMIs);
 
-		// all method declarations: include also the testing
-		Set<String> set2 = testingMIs.keySet();
-		Set<String> tmpSet = null;		
-		for(String s:set2) {
-			testingMD = s;	
-			tmpSet = testingMIs.get(testingMD);	
-			break;						
-		}
+		// Convert to an ordered list of all method declarations to make sure
+		// that the testing method declaration locates at the end of the list
+		List<String> listOfMDs = new ArrayList<>(allMDs);
+		if (listOfMDs.contains(testingMD))
+			listOfMDs.remove(listOfMDs.indexOf(testingMD));
+		listOfMDs.add(testingMD);
 
-		tmpMethodInvocations.putAll(testingMIs);
-		allProjects.put(testingPro, tmpMethodInvocations);
-
-		// convert to an ordered list of all method declarations
-		List<String> listOfMDs = new ArrayList<String>(allMDs);		
-		// to make sure that the testing method declaration locates at the end of the list		
-		if(listOfMDs.contains(testingMD))listOfMDs.remove(listOfMDs.indexOf(testingMD));
-		listOfMDs.add(testingMD);		
-
-		// convert to an ordered list of all method invocations
-		List<String> listOfMIs = new ArrayList<String>(allMIs);
-		// to make sure that all testing method invocations locate at the end of the list
-		for(String testingMI:tmpSet)if(listOfMIs.contains(testingMI))listOfMIs.remove(listOfMIs.indexOf(testingMI));
-		for(String testingMI:tmpSet)listOfMIs.add(testingMI);		
+		// Convert to an ordered list of all method invocations to make sure
+		// that all testing method invocations locate at the end of the list
+		List<String> listOfMIs = new ArrayList<>(allMIs);
+		for (String testingMI : tmpMISet)
+			if (listOfMIs.contains(testingMI))
+				listOfMIs.remove(listOfMIs.indexOf(testingMI));
+		for (String testingMI : tmpMISet)
+			listOfMIs.add(testingMI);
 
 		numOfSlices = listOfPRs.size();
 		numOfRows = listOfMDs.size();
-		numOfCols = listOfMIs.size();						
+		numOfCols = listOfMIs.size();
 
-		byte UserItemContextMatrix[][][] = new byte[this.numOfSlices][this.numOfRows][this.numOfCols];
-		Set<String> myMIs = new HashSet<String>();		
-		String currentMD = "", currentMI = "";
+		byte[][][] matrix = new byte[numOfSlices][numOfRows][numOfCols];
 
-		Map<String,Set<String>> myMDs = new HashMap<String,Set<String>>();
-
-		// populate all cells in the user-item-context ratings matrix using 1 and 0		
-		for(int i=0;i<numOfSlices-1;i++) {			
+		// Populate all cells in the user-item-context ratings matrix using 1s and 0s
+		for (int i = 0; i < numOfSlices - 1; i++) {
 			String currentPro = listOfPRs.get(i);
-			myMDs = allProjects.get(currentPro);
+			Map<String, Set<String>> myMDs = allProjects.get(currentPro);
 
-			for(int j=0;j<numOfRows;j++) {
-				currentMD = listOfMDs.get(j);
-				if(myMDs.containsKey(currentMD)) {
-					myMIs = myMDs.get(currentMD); 
+			for (int j = 0; j < numOfRows; j++) {
+				String currentMD = listOfMDs.get(j);
 
-					for(int k=0;k<numOfCols;k++) {
-						currentMI = listOfMIs.get(k);
-						if(myMIs.contains(currentMI)) {
-							UserItemContextMatrix[i][j][k]=(int) 1.0;
-						}
-						else UserItemContextMatrix[i][j][k]=(int) 0.0;
-					}				
-				} 
-				else	{
-					for(int k=0;k<numOfCols;k++) UserItemContextMatrix[i][j][k]=(int) 0.0;					
+				if (myMDs.containsKey(currentMD)) {
+					Set<String> myMIs = myMDs.get(currentMD);
+
+					for (int k = 0; k < numOfCols; k++) {
+						String currentMI = listOfMIs.get(k);
+
+						if (myMIs.contains(currentMI))
+							matrix[i][j][k] = (byte) 1;
+						else
+							matrix[i][j][k] = (byte) 0;
+					}
+				} else {
+					for (int k = 0; k < numOfCols; k++)
+						matrix[i][j][k] = (byte) 0;
 				}
 			}
 		}
 
-		// this is the testing project, it is the last slice of the 3-D matrix
-		myMDs = allProjects.get(testingPro);
+		// This is the testing project, ie. the last slice of the 3-D matrix
+		Map<String, Set<String>> myMDs = allProjects.get(testingPro);
+		for (int j = 0; j < numOfRows - 1; j++) {
+			String currentMD = listOfMDs.get(j);
 
-		for(int j=0;j<numOfRows-1;j++) {						
-			currentMD = listOfMDs.get(j);	
-			if(myMDs.containsKey(currentMD)) {
-				myMIs = myMDs.get(currentMD);	
+			if (myMDs.containsKey(currentMD)) {
+				Set<String> myMIs = myMDs.get(currentMD);
 
-				for(int k=0;k<numOfCols;k++) {
-					currentMI = listOfMIs.get(k);
-					if(myMIs.contains(currentMI)) {
-						UserItemContextMatrix[numOfSlices-1][j][k]=(int) 1.0;
-					}
-					else UserItemContextMatrix[numOfSlices-1][j][k]=(int) 0.0;
-				}			
-			} 
-			else {
-				for(int k=0;k<numOfCols;k++) UserItemContextMatrix[numOfSlices-1][j][k]=(int) 0.0;					
-			}		
-		}
+				for (int k = 0; k < numOfCols; k++) {
+					String currentMI = listOfMIs.get(k);
 
-		currentMD = listOfMDs.get(numOfRows-1);
-		myMIs = myMDs.get(currentMD);
-		for(int k=0;k<numOfCols;k++) {
-			currentMI = listOfMIs.get(k);
-
-			if(myMIs.contains(currentMI)) {
-				UserItemContextMatrix[numOfSlices-1][numOfRows-1][k]=(int) 1.0;
+					if (myMIs.contains(currentMI))
+						matrix[numOfSlices - 1][j][k] = (byte) 1;
+					else
+						matrix[numOfSlices - 1][j][k] = (byte) 0;
+				}
+			} else {
+				for (int k = 0; k < numOfCols; k++)
+					matrix[numOfSlices - 1][j][k] = (byte) 0;
 			}
-			else UserItemContextMatrix[numOfSlices-1][numOfRows-1][k]=(int) -1.0;			
 		}
 
-		for(String l:listOfPRs)listOfProjects.add(l);
-		for(String l:listOfMIs)listOfMethodInvocations.add(l);
+		String currentMD = listOfMDs.get(numOfRows - 1);
+		Set<String> myMIs = myMDs.get(currentMD);
+		for (int k = 0; k < numOfCols; k++) {
+			String currentMI = listOfMIs.get(k);
 
-		return UserItemContextMatrix;		
+			if (myMIs.contains(currentMI))
+				matrix[numOfSlices - 1][numOfRows - 1][k] = (byte) 1;
+			else
+				matrix[numOfSlices - 1][numOfRows - 1][k] = (byte) -1;
+		}
+
+		for (String l : listOfPRs)
+			listOfProjects.add(l);
+
+		for (String l : listOfMIs)
+			listOfMethodInvocations.add(l);
+
+		return matrix;
 	}
 
-
 	/**
-	 * Recommend new invocations for every testing project
-	 * using the collaborative-filtering technique
+	 * Recommend new invocations for every testing project using the
+	 * collaborative-filtering technique
 	 */
 	public void recommendation() {
-		Map<Integer, String> testingProjects =
-				reader.readProjectList(this.srcDir + "List.txt", testingStartPos, testingEndPos);
+		Map<Integer, String> testingProjects = reader.readProjectList(srcDir + "List.txt", testingStartPos,
+				testingEndPos);
 
 		for (String testingPro : testingProjects.values()) {
 			Map<String, Float> recommendations = new HashMap<>();
@@ -221,13 +204,14 @@ public class ContextAwareRecommendation {
 
 			Map<String, Float> mdSimScores = new HashMap<String, Float>();
 
-			// Compute the jaccard similarity between the testingMethod and every other method
+			// Compute the jaccard similarity between the testingMethod and every other
+			// method
 			// and store the results in mdSimScores
 			for (int i = 0; i < numOfSlices - 1; i++) {
 				for (int j = 0; j < numOfRows; j++) {
 					byte[] otherMethodVector = matrix[i][j];
-					
-					SimilarityCalculator simCalculator = new SimilarityCalculator(this.srcDir);
+
+					SimilarityCalculator simCalculator = new SimilarityCalculator(srcDir);
 					float sim = simCalculator.computeJaccardSimilarity(testingMethodVector, otherMethodVector);
 					String key = Integer.toString(i) + "#" + Integer.toString(j);
 					mdSimScores.put(key, sim);
@@ -295,19 +279,4 @@ public class ContextAwareRecommendation {
 			reader.writeRecommendations(recDir + testingPro, recSortedMap, recommendations);
 		}
 	}
-
-
-	public double CosineSimilarity(double[] vector1, double[] vector2) {        
-		double sclar = 0, norm1 = 0, norm2 = 0;
-		int length = vector1.length;       
-		for(int i=0;i<length;i++) sclar+=vector1[i]*vector2[i];
-		for(int i=0;i<length;i++) norm1+=vector1[i]*vector1[i];
-		for(int i=0;i<length;i++) norm2+=vector2[i]*vector2[i];
-		double ret = 0;
-		double norm = norm1*norm2;                
-		if(norm>0 && sclar>0) ret = (double)sclar / Math.sqrt(norm);        
-		else ret =0;
-		return ret;
-	}
-
 }
