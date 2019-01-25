4579ad8594006247ba2169e00d33bf1761a137ae
tdegueul <degueule@cwi.nl>
2019-01-25 11:34:48 +0100

    Make run-all.sh a little more verbose

--- tools/Focus/run-all.sh
+++ tools/Focus/run-all.sh
@@ -2,7 +2,12 @@
 
 JAR=$PWD/target/focus-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 
-for f in $(ls confs);
-	do java -Xmx8g -jar $JAR confs/$f > $PWD/results/$f.txt
+for f in $(ls confs); do
+	CONF=$PWD/confs/$f
+	OUT=$PWD/results/$f.txt
+
+	echo "Running FOCUS on configuration $CONF. Outputs are written to $OUT."
+	java -Xmx8g -jar $JAR $CONF > $OUT
+	echo "Done"
 done
 
