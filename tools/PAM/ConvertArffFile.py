5384ea4664b58849fae11e43e938cc0da854c699
md2manoppello <juri.di.rocco@gmail.com>
2019-01-25 18:23:48 +0100

    Changed PAM extractor scripts

--- tools/PAM/ConvertArffFile.py
+++ tools/PAM/ConvertArffFile.py
@@ -1,40 +1,44 @@
 import os
 
-lpath = "C://Users//claudio//Desktop//focusToPam//"
-lpath2 = "C://Users//claudio//Desktop//dataset//"
+#lpath = "C://Users//claudio//Desktop//focusToPam//"
+#lpath2 = "C://Users//claudio//Desktop//dataset//"
 
-c=0
-#per claudio, prende i file con il formato arff errato e li rigenera correttamente
-
-for f in os.listdir(lpath):
-    my_dict = {}
-    with open(os.path.join(lpath, f),"r",encoding='utf-8', errors='ignore') as file:
-        count = 0
-        for line in file:
-            if count > 2:
-                partLine = line.split("','")
-                md = partLine[0]
-                mi = partLine[1]
-                md = md[2:]
-                mi = mi[:-2]
-                if md in my_dict:
-                    l = my_dict[md]
-                    l.append(mi)
-                else:
-                    my_dict[md] = [mi]
-            count = count +1
-    result = ""
-    for key, value in my_dict.items():
-        result = result + "'" + key + "','"
-        for element in value:
-            result = result + element + " "
-        result = result[:-2] + "'\n"
-    #with open(os.path.join(lpath2, f), "w") as file:
-    i = f.find(".txt")
-    f = f[:i]
-    with(open(lpath2+f+".arff","w",encoding='utf-8', errors='ignore')) as file:
-        file.write(result)
-
+def run(inputPath, outputPath):
+    c=0
+    #per claudio, prende i file con il formato arff errato e li rigenera correttamente
+    files = (file for file in os.listdir(inputPath)
+             if os.path.isfile(os.path.join(inputPath, file)))
+    for f in files:
+        my_dict = {}
+        try:
+            with open(os.path.join(inputPath, f), "r", encoding='utf-8', errors='ignore') as file:
+                count = 0
+                for line in file:
+                    if count > 2:
+                        partLine = line.split("','")
+                        md = partLine[0]
+                        mi = partLine[1]
+                        md = md[2:]
+                        mi = mi[:-2]
+                        if md in my_dict:
+                            l = my_dict[md]
+                            l.append(mi)
+                        else:
+                            my_dict[md] = [mi]
+                    count = count +1
+            result = ""
+            for key, value in my_dict.items():
+                result = result + "'" + key + "','"
+                for element in value:
+                    result = result + element + " "
+                result = result[:-2] + "'\n"
+            #with open(os.path.join(lpath2, f), "w") as file:
+            i = f.find(".txt")
+            f = f[:i]
+            with(open(outputPath + f + ".arff", "w", encoding='utf-8', errors='ignore')) as file:
+                file.write(result)
+        except:
+            print ('error ' + f)
 
 
 
--- tools/PAM/parsingPAM.py
+++ tools/PAM/parsingPAM.py
@@ -1,16 +1,21 @@
 import os
+import sys
+import ConvertArffFile
+
 
 
 def splitFiles(PAM_path,part_path,part2_path):
 
     #PAM_path="C://Users//claudio//Desktop//dataset//"
-    for f in os.listdir(PAM_path):
-        print(f)
+    files = (file for file in os.listdir(PAM_path)
+             if os.path.isfile(os.path.join(PAM_path, file)))
+    for f in files:
+
         file = open(PAM_path+f,"r",encoding='utf-8', errors='ignore')
         lines=file.readlines()
         tot=len(lines)
         rmv=(tot*2)/3
-        print(int(rmv))
+
         part = open(part_path + f, "w",encoding='utf-8', errors='ignore')
         part2= open(part2_path + f, "w",encoding='utf-8', errors='ignore')
         i=0
@@ -23,11 +28,8 @@ def splitFiles(PAM_path,part_path,part2_path):
                 break
         i=rmv
         for line2 in lines:
-
             i+=1
             c+=1
-
-            print(c)
             if(c>=int(rmv)):
                 part2.write(line2)
                 if(c>=tot):
@@ -42,14 +44,10 @@ def combineMethodsInv(root_invocations,root_testing,root_arff):
 
     for f in os.listdir(root_invocations):
         file=open(root_invocations+f,"r",encoding='utf-8', errors='ignore')
-        thisFile = os.path.basename(file.name)
-        i = thisFile.find(".arff")
-        thisFile = thisFile[:i]
-        file2=open(root_testing+thisFile+".txt","r",encoding='utf-8', errors='ignore')
-        thisFile = os.path.basename(file2.name)
-        j = thisFile.find(".txt")
-        thisFile = thisFile[:j]
-        result_arff = open(root_arff+thisFile+".arff","w", encoding='utf-8', errors='ignore')
+
+        file2=open(os.path.join(root_testing, f),"r",encoding='utf-8', errors='ignore')
+
+        result_arff = open(os.path.join(root_arff,f),"w", encoding='utf-8', errors='ignore')
         result_arff.write("@relation project\n @attribute fqCaller string \n  @attribute fqCalls string \n @data \n")
         for e in file:
             result_arff.write(e)
@@ -62,22 +60,24 @@ def combineAll(start,end,dataset):
     #end="C://Users//claudio//Desktop//second_run//PAM_arff//"
     for file in os.listdir(start):
         ff =open(start+file,"r",encoding="utf-8",errors="ignore")
-        resultArff=open(end+file,"w",encoding="utf-8",errors="ignore")
+        resultArff=open(os.path.join(end,file),"w",encoding="utf-8",errors="ignore")
         for e in ff:
             resultArff.write(e)
         #folders="C://Users//claudio//Desktop//dataset//"
         for e in os.listdir(dataset):
-            file=open(dataset+e,"r",encoding="utf-8",errors="ignore")
-            for row in file:
-                resultArff.write(row)
+            if os.path.isfile(os.path.join(dataset, e)):
+                file=open(os.path.join(dataset, e),"r",encoding="utf-8",errors="ignore")
+                for row in file:
+                    resultArff.write(row)
 
 
 
 def fromFocusToPam(dirFocus,dest):
     #dirFocus="C://Users//claudio//Desktop//PAM_dataset//"
     #dest = "C://Users//claudio//Desktop//focusToPam//"
-
-    for f in os.listdir(dirFocus):
+    files = (file for file in os.listdir(dirFocus)
+             if os.path.isfile(os.path.join(dirFocus, file)))
+    for f in files:
         file = open(dirFocus +f,"r",encoding='utf-8', errors='ignore')
         name=os.path.basename(file.name)
 
@@ -92,28 +92,27 @@ def getGroundTruth(arff_path,invocations_path,groundt_path):
     #PAM_path = "C://Users//claudio//Desktop//focus_d//"
     total_row = 0
     for f in os.listdir(arff_path):
-        print(f)
-        file = open(arff_path + f, "r",encoding='utf-8', errors='ignore')
-        lines = file.readlines()
-        tot = len(lines)
-        part = open(invocations_path + f, "w",encoding='utf-8', errors='ignore')
-        part2 = open(groundt_path + f, "w",encoding='utf-8', errors='ignore')
-        i = 0
-        c = 0
-        for line in lines:
-            part.write(line)
-            i += 1
-            if (i >= 3):
-                break
-
-        for line2 in lines:
-            i += 1
-            c += 1
-            print(c)
-            if (c >= 3):
-                part2.write(line2)
-                if (c >= tot):
+        if os.path.isfile(os.path.join(arff_path,f)):
+            file = open(os.path.join(arff_path,f), "r",encoding='utf-8', errors='ignore')
+            lines = file.readlines()
+            tot = len(lines)
+            part = open(os.path.join(invocations_path,f), "w",encoding='utf-8', errors='ignore')
+            part2 = open(os.path.join(groundt_path,f), "w",encoding='utf-8', errors='ignore')
+            i = 0
+            c = 0
+            for line in lines:
+                part.write(line)
+                i += 1
+                if (i >= 3):
                     break
+
+            for line2 in lines:
+                i += 1
+                c += 1
+                if (c >= 3):
+                    part2.write(line2)
+                    if (c >= tot):
+                        break
 
 
 
@@ -143,10 +142,49 @@ def recommendationFormat(path,destPath):
         for i,j in zip(list_rec,list_prob):
             dest.write(i.replace("\n","  "))
             dest.write(j)
+#focus_path = '/home/juri/git/FOCUS/dataset/SH_L/'
+#pam_path = '/home/juri/Scrivania/output/PAM/'
+#fromFocusToPam(focus_path, pam_path)
+#splitFiles(pam_path, '/home/juri/Scrivania/output/2_3/', '/home/juri/Scrivania/output/1_3/')
 
+def removeTempFiles(pam_path):
+    files = (file for file in os.listdir(pam_path)
+             if file.endswith(".txt"))
+    for f in files:
+        os.remove(os.path.join(pam_path, f))
 
+def cleanFolder(myPath):
+    for root, dirs, files in os.walk(myPath):
+        for file in files:
+            os.remove(os.path.join(root, file))
 
+def createFolder(myPath):
+    if not os.path.exists(myPath):
+        os.makedirs(myPath)
 
+def run(focus_path,pam_path):
+    cleanFolder(pam_path)
+    createFolder(os.path.join(pam_path,'2_3/'))
+    createFolder(os.path.join(pam_path, '1_3/'))
+    createFolder(os.path.join(pam_path, 'RP/'))
+    createFolder(os.path.join(pam_path, 'GT/'))
+    createFolder(os.path.join(pam_path, 'RESULT/'))
+    createFolder(os.path.join(pam_path, 'DESTINATION/'))
+    print("Generating temp files")
+    fromFocusToPam(focus_path, pam_path)
+    print('Converting to arff format')
+    ConvertArffFile.run(pam_path,pam_path)
+    print('Remove temp files')
+    removeTempFiles(pam_path)
+    print ('Split dataset in 1/3 and 2/3 partitions')
+    splitFiles(pam_path, os.path.join(pam_path,'2_3/'), os.path.join(pam_path,'1_3/'))
+    print ('Compute GT')
+    getGroundTruth(pam_path, os.path.join(pam_path,'RP/'), os.path.join(pam_path,'GT/'))
+    combineMethodsInv(os.path.join(pam_path,'2_3/'), os.path.join(pam_path,'RP/'), os.path.join(pam_path, "RESULT/"))
+    combineAll(os.path.join(pam_path,"RESULT/"), os.path.join(pam_path,"DESTINATION/"),pam_path)
 
-
+if __name__ == "__main__":
+    focus_path = '/home/juri/Scrivania/input/'#sys.argv[1]
+    pam_path = '/home/juri/Scrivania/output/PAM/'#sys.argv[2]
+    run(focus_path, pam_path)
 
